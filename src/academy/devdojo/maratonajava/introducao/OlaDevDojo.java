55f1b37db03c89949e56c28beee89e095c194e12
Silvio Franco <silviofrancoms@gmail.com>
2022-07-16 01:12:27 -0400

    Comentários, atalhos sobre documentação dos métodos

---
+++ src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário javadoc
+ *
+ */
+
+public class OlaDevDojo {
+    // Isso é um comentário de uma linha
+
+
+    /*
+       - Isso é um comentário
+         de multiplas
+          linhas
+
+
+          - ctrl + alt + q          ->   Expande/Retrai JavaDoc
+
+          - ctrl + q          ->   Expande infos de documentação sobre algum método, tal qual deixar o mouse em cima do método
+
+          - - ctrl + b          ->   ir até a implementação tal qual ctrl + clique no método
+
+    */
+
+    public static void main(String[] args) {
+        System.out.println("KA-ME-HA-ME-HA");
+    }
+}
